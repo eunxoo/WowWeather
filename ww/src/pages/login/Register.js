beafeedde177b06f1abe5d5d7cbdc0a51ee74a3a
eunxoo <dmstn7989@naver.com>
2023-08-07 20:50:07 +0900

    [feat] 회원가입 에러 && 뒤로가기

--- ww/src/pages/login/Register.js
+++ ww/src/pages/login/Register.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   firebaseAuth,
   createUserWithEmailAndPassword,
+  sendEmailVerification,
   updateProfile,
 } from "../../fbase";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { FaArrowLeft } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   const [registerEmail, setRegisterEmail] = useState("");
@@ -14,6 +16,9 @@ const Register = () => {
   const [errorMsg1, setErrorMsg1] = useState(" ");
   const [errorMsg2, setErrorMsg2] = useState(" ");
   const [errorMsg3, setErrorMsg3] = useState(" ");
+  const [isName, setIsName] = useState(true);
+
+  const navigate = useNavigate();
 
   const onChange = (event) => {
     const {
@@ -23,18 +28,26 @@ const Register = () => {
       setRegisterEmail(value);
     } else if (name === "password") {
       setRegisterPassword(value);
+    } else if (name === "displayName") {
+      setDisplayName(value);
     }
   };
+
   const signup = async () => {
     try {
       setErrorMsg1(" ");
       setErrorMsg2(" ");
       setErrorMsg3(" ");
+      if (displayName == " ") {
+        setIsName(false);
+      }
       const createdUser = await createUserWithEmailAndPassword(
         firebaseAuth,
         registerEmail,
         registerPassword
       );
+      await sendEmailVerification(firebaseAuth.currentUser);
+      alert("auth/post-email-verification-mail");
       console.log(createdUser);
       setRegisterEmail("");
       setRegisterPassword("");
@@ -42,9 +55,10 @@ const Register = () => {
       await updateProfile(firebaseAuth.currentUser, {
         displayName: displayName,
       });
-      window.location.replace("/login");
+      window.location.replace("/");
     } catch (err) {
       console.log(err.code);
+      // setIsError(true);
       switch (err.code) {
         case "auth/weak-password":
           setErrorMsg2("* 비밀번호는 6자리 이상이어야 합니다.");
@@ -58,50 +72,81 @@ const Register = () => {
       }
     }
   };
+
   return (
     <Container>
-      <FaArrowLeft />
-      <Box>
-        <SemiBox>
-          <AText>아이디</AText>
-          <AInput
-            name="email"
-            value={registerEmail}
-            placeholder="이메일을 입력해주세요"
-            onChange={(e) => {
-              setRegisterEmail(e.target.value);
-            }}
-          ></AInput>
-          <ErrMsg1>{errorMsg1}</ErrMsg1>
-        </SemiBox>
-        <SemiBox>
-          <AText>비밀번호</AText>
-          <AInput
-            name="password"
-            value={registerPassword}
-            placeholder="비밀번호를 입력해주세요"
-            onChange={(e) => {
-              setRegisterPassword(e.target.value);
-            }}
-          ></AInput>
-          <ErrMsg2>{errorMsg2}</ErrMsg2>
-        </SemiBox>
-        <SemiBox>
-          <AText>닉네임</AText>
-          <AInput
-            name="displayName"
-            value={displayName}
-            placeholder="닉네임을 입력해주세요"
-            onChange={(e) => {
-              setDisplayName(e.target.value);
-            }}
-          ></AInput>
-        </SemiBox>
-        <BtnBox>
-          <ValidBtn onClick={signup}>회원가입</ValidBtn>
-          <ErrMsg3>{errorMsg3}</ErrMsg3>
-        </BtnBox>
-      </Box>
+      <Wrap>
+        <Header>
+          <FaArrowLeft className="arrow" onClick={() => navigate("/")} />
+        </Header>
+        <Box>
+          <SemiBox>
+            <AText>이메일</AText>
+            {errorMsg1 == " " ? (
+              <AInput
+                name="email"
+                value={registerEmail}
+                placeholder="이메일을 입력해주세요"
+                onChange={onChange}
+              ></AInput>
+            ) : (
+              <ErrorInput
+                name="email"
+                value={registerEmail}
+                placeholder="이메일을 입력해주세요"
+                onChange={onChange}
+              ></ErrorInput>
+            )}
+            <ErrMsg1>{errorMsg1}</ErrMsg1>
+          </SemiBox>
+          <SemiBox>
+            <AText>비밀번호 (6자리 이상 입력해주세요.) </AText>
+            {errorMsg2 == " " ? (
+              <AInput
+                name="password"
+                value={registerPassword}
+                placeholder="비밀번호를 입력해주세요"
+                onChange={onChange}
+              ></AInput>
+            ) : (
+              <ErrorInput
+                name="password"
+                value={registerPassword}
+                placeholder="비밀번호를 입력해주세요"
+                onChange={onChange}
+              ></ErrorInput>
+            )}
+            <ErrMsg2>{errorMsg2}</ErrMsg2>
+          </SemiBox>
+          <SemiBox>
+            <AText>닉네임</AText>
+            {isName ? (
+              <AInput
+                name="displayName"
+                value={displayName}
+                placeholder="닉네임을 입력해주세요"
+                onChange={onChange}
+              ></AInput>
+            ) : (
+              <ErrorInput
+                name="displayName"
+                value={displayName}
+                placeholder="닉네임을 입력해주세요"
+                onChange={onChange}
+              ></ErrorInput>
+            )}
+          </SemiBox>
+          <BtnBox>
+            {!registerEmail || !registerPassword || !displayName ? (
+              <InvalidBtn>회원가입</InvalidBtn>
+            ) : (
+              <ValidBtn onClick={signup}>회원가입</ValidBtn>
+            )}
+
+            <ErrMsg3>{errorMsg3}</ErrMsg3>
+          </BtnBox>
+        </Box>
+      </Wrap>
     </Container>
   );
 };
@@ -119,7 +164,30 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-items: center;
+
+  .arrow {
+    position: absolute;
+  }
 `;
+
+const Wrap = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+const Header = styled.header`
+  position: fixed;
+  top: -22vh;
+  left: 4vw;
+  .arrow {
+    position: absolute;
+    font-size: 3vh;
+    color: #31b5ff;
+  }
+`;
+
 const Box = styled.div`
   display: flex;
   flex-direction: column;
@@ -136,6 +204,29 @@ const SemiBox = styled.div`
 
 const AText = styled.div`
   margin-bottom: 2vh;
+  font-size: 1.5vh;
+`;
+
+const ErrorInput = styled.input`
+  border: 1px solid red;
+  border-radius: 10px;
+  outline: none;
+  height: 5vh;
+  width: 70vw;
+  margin-bottom: 6vh;
+
+  &::placeholder {
+    padding-left: 10px;
+  }
+
+  /* &:hover {
+    border: 2px solid grey;
+  } */
+
+  &:focus {
+    /* color: #363636; */
+    border: 3px solid #31b5ff;
+  }
 `;
 
 const AInput = styled.input`
@@ -184,6 +275,18 @@ const BtnBox = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+const InvalidBtn = styled.div`
+  border: 2px solid #31b5ff;
+  background-color: grey;
+  padding: 0.5vh 3vw;
+  border-radius: 10px;
+  width: 70%;
+  height: 4vh;
+  text-align: center;
+  line-height: 4vh;
+  font-weight: bold;
 `;
 
 const ValidBtn = styled.div`
