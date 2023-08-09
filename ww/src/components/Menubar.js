68841cd5838c74df1131b9d86b7b5bfdfeefd650
eunxoo <dmstn7989@naver.com>
2023-08-09 22:16:34 +0900

    [feat] 네이게이션 바(햄버거) 구현

--- ww/src/components/Menubar.js
+++ ww/src/components/Menubar.js
@@ -1,12 +1,101 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+import { GiHamburgerMenu } from "react-icons/gi";
+import Sidebar from "./Sidebar";
 
-const Menubar = () => {
+const Menubar = ({ userObj }) => {
+  const [isShow, setIsShow] = useState(false);
+
+  const controlNavbar = () => {
+    if (window.scrollY > 50) {
+      setIsShow(true);
+    } else {
+      setIsShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, []);
+
+  const showModal = (e) => {
+    document.body.style.overflow = "hidden";
+  };
+
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleSide = () => {
+    setIsOpen(true);
+    setIsShow(true);
+    showModal();
+  };
   return (
-    <div>
-      ㅎ<Outlet />
-    </div>
+    <Container>
+      <Wrap>
+        <Navbar className={`nav ${isShow && "navNone"}`}></Navbar>
+        <GiHamburgerMenu
+          className={`hamburger ${isShow && "hamburgerNone"}`}
+          role="button"
+          onClick={toggleSide}
+        />
+      </Wrap>
+      <Outlet />
+      <Sidebar
+        setIsShow={setIsShow}
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        userObj={userObj}
+      />
+    </Container>
   );
 };
 
 export default Menubar;
+
+const Container = styled.div`
+  position: relative;
+  .navNone {
+    height: 30px;
+    width: 100%;
+    display: none;
+    visibility: none;
+    opacity: 1;
+  }
+`;
+
+const Wrap = styled.div`
+  height: 5vh;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  padding-bottom: 1vh;
+  transition-timing-function: ease-in;
+  transition: 0.5s;
+
+  .hamburger {
+    position: absolute;
+    top: 1vh;
+    left: 3vw;
+    font-size: 4vh;
+    z-index: 5;
+  }
+
+  .hamburgerNone {
+    display: none;
+  }
+`;
+
+const Navbar = styled.div`
+  background-color: #c2e9ff;
+  opacity: 0.5;
+  height: 5vh;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  padding-bottom: 1vh;
+  transition-timing-function: ease-in;
+  transition: 0.5s;
+`;
---
+++ ww/src/components/Sidebar.js
@@ -0,0 +1,132 @@
+import React, { useEffect, useRef } from "react";
+import { MdClose, MdNavigateNext } from "react-icons/md";
+import styled from "styled-components";
+import { Link, useLocation } from "react-router-dom";
+import { firebaseAuth, signOut } from "../fbase";
+
+const Sidebar = ({ setIsShow, isOpen, setIsOpen, userObj }) => {
+  const location = useLocation();
+  useEffect(() => {
+    document.addEventListener("mousedown", handlerOutside);
+    return () => {
+      document.removeEventListener("mousedown", handlerOutside);
+    };
+  }, []);
+  const toggleSide = () => {
+    setIsOpen(false);
+    setIsShow(false);
+    hideModal();
+  };
+
+  const outside = useRef();
+  const handlerOutside = (e) => {
+    if (!outside.current.contains(e.target)) {
+      toggleSide();
+      hideModal();
+    }
+  };
+
+  const hideModal = (e) => {
+    document.body.style.overflow = "unset";
+  };
+
+  const logout = async () => {
+    await signOut(firebaseAuth);
+    window.location.replace("/");
+  };
+
+  return (
+    <Container className={isOpen ? "open" : ""} ref={outside}>
+      <MdClose className="close" onClick={toggleSide} />
+      <ul className="ul">
+        <Menu isActive={location.pathname === "/"}>
+          <MdNavigateNext className="in" />
+          <Link className="link" to="/" onClick={toggleSide}>
+            홈
+          </Link>
+        </Menu>
+        <Menu isActive={location.pathname === "/checklist"}>
+          <MdNavigateNext className="in" />
+          <Link className="link" to="/checklist" onClick={toggleSide}>
+            체크리스트
+          </Link>
+        </Menu>
+        <Menu isActive={location.pathname === "/mystyle"}>
+          <MdNavigateNext className="in" />
+          <Link className="link" to="/mystyle" onClick={toggleSide}>
+            나의 스타일
+          </Link>
+        </Menu>
+      </ul>
+      <SignOut onClick={logout}>로그아웃</SignOut>
+      <Box>{userObj.displayName + "님, 안녕하세요!"}</Box>
+    </Container>
+  );
+};
+
+export default Sidebar;
+
+const Container = styled.div`
+  z-index: 5;
+  position: fixed;
+  left: -65%;
+  top: 0;
+  height: 100%;
+  width: 60%;
+  background-color: #c2e9ff;
+  padding: 2%;
+  transition: 0.5s ease;
+
+  &.open {
+    left: 0;
+    top: 0;
+    transition: 0.5s ease;
+    height: 100%;
+  }
+
+  .close {
+    font-size: 4vh;
+  }
+
+  .ul {
+    list-style: none;
+    padding-left: 0;
+    margin: 6vh 0.5vw;
+  }
+`;
+const Menu = styled.li`
+  .link {
+    text-decoration-line: none;
+    color: black;
+    list-style: none;
+  }
+
+  margin-bottom: 3vh;
+  margin-left: ${(props) =>
+    props.isActive ? "3vw" : "0"}; // 주소가 일치할 때만 들여쓰기를 적용
+  /* transition: padding-left 0.3s ease-in-out; */
+
+  /* &:hover {
+    margin-left: 3vw; // 마우스 호버시 들여쓰기 적용
+  } */
+
+  .in {
+    position: relative;
+    top: 2px;
+  }
+`;
+
+const SignOut = styled.div`
+  position: absolute;
+  bottom: 7vh;
+  left: 3vw;
+  border: 1px solid black;
+  border-radius: 10px;
+  background-color: white;
+  padding: 1vh 1vw;
+`;
+const Box = styled.div`
+  position: absolute;
+  bottom: 3.5vh;
+  left: 3vw;
+`;
